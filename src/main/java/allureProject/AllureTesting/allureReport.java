219236505f958f8b39f522e3769cffc86a746e62
Amar-Pameshwari <parmeshwari.amar@yahoo.com>
2021-08-20 18:15:01 +0530

    first commit

---
+++ src/main/java/allureProject/AllureTesting/allureReport.java
@@ -0,0 +1,14 @@
+package allureProject.AllureTesting;
+
+import static org.testng.Assert.assertTrue;
+
+import org.testng.annotations.Test;
+
+public class allureReport {
+
+	@Test
+	public static void reportTesting() {
+		
+		assertTrue(true);
+	}
+}
